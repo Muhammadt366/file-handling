77d4409c1d82fbe1678e6c802e017d3f1e1f97ba
Muhammadt366 <muhammadbinm22@gmail.com>
2026-08-17 19:59:35 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-file = open('Codingal.txt')
+file_read = open('Codingal.txt', 'r')
+print("file in read mode...")
+print(file_read.read())
+file_read.close()
 
+file_write = open('Codingal.txt', 'w')
+file_write.write("File in write mode-")
+file_write.write("Hi! i am a penguin i am 10 yrs old")
+file_write.close()
 
-
-print(file.read())
-
-file.close()
+file_append = open('Codingal.txt', 'a')
+file_append.write("\n File in append mode...")
+file_append.write("Hi! i am a penguin i am 10 yrs old")
+file_append.close()
